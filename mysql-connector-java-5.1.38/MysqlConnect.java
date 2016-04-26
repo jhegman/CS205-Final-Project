e9740c6a9e3fe4f154562d063e70efa3769648b1
Gretchen <ggirdzis@uvm.edu>
2016-04-26 19:44:32 -0400

    Clean SQL Code

--- mysql-connector-java-5.1.38/MysqlConnect.java
+++ mysql-connector-java-5.1.38/MysqlConnect.java
@@ -1,47 +1,53 @@
+//Import statements
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.*;
 import java.util.Date;
 import java.util.Calendar;
 import java.sql.Timestamp;
-
 import java.util.ResourceBundle;
 
+/**
 
+   Created by Gretchen. Based on Robert Erickson's code. This class connects to SQL Database, and reads in data passed
+   in as statistics from the game Sorry.
+
+*/
 
 public class MysqlConnect{
+
+      /**
+      
+         This constructor takes in game variables, and syncs them with the database.
+      
+      */
 
 
       public MysqlConnect(String theName, boolean theValue, String theWinner, int theMoves)
       {
+         
          //Variables
          String name = theName;
          boolean value = theValue;
          String winner = theWinner;
          int moves = theMoves;
          
-         
-         
-      
-   	   //public static void main(String[] args) {
-   		// System.out.println("MySQL Connect Example.");
+         //Set connection to null
    		Connection conn = null;
          Statement stmt = null;
          
+         //Time & date stamp
          java.util.Date thedate= new java.util.Date();
    	   Timestamp date = new Timestamp(thedate.getTime());
          
             
      
-   		//ResourceBundle properties = ResourceBundle.getBundle("MysqlConnect");
+   		//SQL Information
    		String url = "jdbc:mysql://webdb.uvm.edu/";
    		String dbName = "GGIRDZIS_CS205";
    		String driver = "com.mysql.jdbc.Driver";
@@ -50,26 +56,22 @@ public class MysqlConnect{
    
    
       
+         //Connection for database
    		try {
    			Class.forName(driver).newInstance();
    			conn = DriverManager.getConnection(url + dbName, userName, password);
             
-            
-            
-                      
+                     
             stmt = conn.createStatement();
          
+            //Insert statement
             String sql = "INSERT INTO tblPlayer " +
                       "VALUES ('"+name+"'," +value+", '"+winner+"', "+moves+", '"+date+"')";
+            
+            //Update
             stmt.executeUpdate(sql);
              
-            //stmt.setString(1, "Name");
-            //stmt.setDate(2, timestamp);
-            //stmt.setBoolean(2, true);
-            //stmt.setString(3, "Player");
-           // stmt.setInt(4, 10);
-                      
-            stmt.executeUpdate(sql);          
+         //Exception           
    		} catch (Exception e) {
    			System.err.println("Error MysqlConnect.myConnect: " + e.getMessage());
    			e.printStackTrace();
@@ -77,7 +79,7 @@ public class MysqlConnect{
    		
        }  
       
-	//}
+	
 }
 
             
